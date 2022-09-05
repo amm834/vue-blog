d5b46a0326b8c00537dee84c83c81bf8108f4c29
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-06 02:18:48 +0630

    init: initial commit 👻

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import Timeline from "./components/Timeline.vue";
+</script>
+
+<template>
+
+  <div class="container">
+    <Timeline />
+  </div>
+</template>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <ul class="nav justify-content-center mt-3 shadow-sm rounded-2">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="#">Tody</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="#">This Week</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="#">This Month</a>
+    </li>
+  </ul>
+</template>
