4a95530418e73b368da61eee1f427c425e32e399
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-07 01:38:55 +0630

    test: test timeline with suspense

--- src/components/__tests__/TimeLine.spec.ts
+++ src/components/__tests__/TimeLine.spec.ts
@@ -1,20 +1,55 @@
 import TimeLine from "../TimeLine.vue";
-import { mount } from "@vue/test-utils";
-import { describe, it } from "vitest";
+import { mount, flushPromises } from "@vue/test-utils";
+import { describe, it, vi } from "vitest";
 import { expect } from "vitest";
 import { thisMonth, thisWeek, today } from "../../mocks";
-import { nextTick } from "vue";
+import { beforeAll, beforeEach } from "vitest/dist/browser";
+
+vi.mock("axios", () => ({
+  default: {
+    get: (url: string) => {
+      if (url === "/posts") {
+        return Promise.resolve({
+          data: [today, thisWeek, thisMonth]
+        });
+      }
+    }
+  }
+}));
+
+
+const mountTimeLine = () => {
+  return mount({
+    components: { TimeLine },
+    template: `
+      <Suspense>
+      <template #default>
+        <TimeLine />
+      </template>
+      <template #fallback>
+        Loading..
+      </template>
+      </Suspense>
+    `
+  });
+};
 
 describe("TimeLine", () => {
-  it("should render today default post", function() {
-    const wrapper = mount(TimeLine);
+  it("should render today default post", async function() {
+
+    const wrapper = mountTimeLine();
+
+    // resolve the external promises
+    await flushPromises();
 
     expect(wrapper.html()).toContain(today.created.format("Do MMM"));
   });
 
 
   it("should render when period today was clicked", async () => {
-    const wrapper = mount(TimeLine);
+    const wrapper = mountTimeLine();
+
+    await flushPromises();
 
     //click the period tab
     await wrapper.get("[data-test=\"Today\"]").trigger("click");
@@ -24,7 +59,9 @@ describe("TimeLine", () => {
   });
 
   it("should render when period this week was clicked", async () => {
-    const wrapper = mount(TimeLine);
+    const wrapper = mountTimeLine();
+
+    await flushPromises();
 
     //click the period tab
     await wrapper.get("[data-test=\"This Week\"]").trigger("click");
@@ -34,7 +71,9 @@ describe("TimeLine", () => {
   });
 
   it("should render when period this month was clicked", async () => {
-    const wrapper = mount(TimeLine);
+    const wrapper = mountTimeLine();
+
+    await flushPromises();
 
     //click the period tab
     await wrapper.get("[data-test=\"This Month\"]").trigger("click");
