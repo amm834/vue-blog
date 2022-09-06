4aa2dfdeb595e3833cb43c214552406d5e1c6b69
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-07 02:16:08 +0630

    feat: add PostEditor

--- src/components/NewPost.vue
+++ src/components/NewPost.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+
+import PostEditor from "@/components/PostEditor.vue";
+import type { Post } from "@/mocks";
+import moment from "moment";
+
+const post: Post = {
+  id: "-1",
+  title: "New Post Title",
+  created: moment()
+};
+</script>
 <template>
-  New Post
+  <p class="fw-bold">New Post</p>
+  <PostEditor :new-post="post" />
 </template>
---
+++ src/components/PostEditor.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { Post } from "@/mocks";
+
+defineProps<{
+  newPost: Post
+}>();
+
+</script>
+
+<template>
+  <div>
+    <input type="text"
+           v-model="newPost.title"
+           class="form-control"
+    >
+  </div>
+</template>
+
